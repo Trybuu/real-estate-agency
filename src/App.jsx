3ba3220b069db4f2400749d57690fb50ce58573f
Faiku <faiku@proton.me>
2023-11-08 14:51:56 +0100

    Update gitignore & Fix file case-sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
-import Home from "./components/Home/Main";
+import Home from "./components/Home/main";
 import Navbar from "./components/Navbar/main"
 import Footer from "./components/Footer/main"
 import './App.css'
@@ -10,14 +10,14 @@ function App() {
     <>
       <Router>
         <Navbar/>
-        <Routes> 
-          /* Root path to main route */
-          <Route exact path="/" element={<Home/>} />
-          /* Subsites */
-          <Route exact path="/example" element={<Home/>} />
-          /* 404 error catch */
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+          <Routes> 
+            /* Root path to main route */
+            <Route exact path="/" element={<Home/>} />
+            /* Subsites */
+            <Route exact path="/example" element={<Home/>} />
+            /* 404 error catch, if everything else fails then redirect to root path aka. Main site */
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
         <Footer/>
       </Router>
     </>
